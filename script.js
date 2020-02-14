e1022af8ec377acbb923ac4eb2d199053f2d6ed2
anderson amaya <anderama100@gmail.com>
2020-02-14 03:23:21 -0700

    Finished

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ const AttNameE1 = document.getElementById('name');
 const AttEyesE1 = document.getElementById('eyes');
 const AttHairE1 = document.getElementById('hairColor');
 const AttSizeE1 = document.getElementById('sltsize');
+let dog;
+let goose;
+let cat;
+let paraphE1;
+const familyTypeE2 = document.getElementById('family');
+const noiseE2 = document.getElementById('noise');
+const codedisplay = document.getElementById('footerOut');
+const codedisplay2 = document.getElementById('ppal');
+const createPetBtn = document.querySelector('#createPet');
+const endBtn = document.querySelector('#endBtn');
 
-document.getElementById('Att-Form').addEventListener('submit', DatPet => {
+document.getElementById("dogSk").style.display = "none";
+document.getElementById("dogF").style.display = "none";
 
+////////////////////// F U N C T I O N S//////////////////////////////
+
+/////CREATING NEW INSTANCES
+
+document.getElementById('Att-Form').addEventListener('submit', e => {
+e.preventDefault();
   let AttType = AttTypeE1.value;
   let AttName = AttNameE1.value;
   let AttEyes = AttEyesE1.value;
@@ -14,40 +31,132 @@ document.getElementById('Att-Form').addEventListener('submit', DatPet => {
   let AttSize = AttSizeE1.value;
 
 
-   if(AttType < 2){
+   if(AttType === '1'){
 
-     let dog = new Doggy(AttName, AttEyes, AttHair, AttSize);
-     console.log(dog.AttName);
+     dog = new Doggy(AttName, AttEyes, AttHair, AttSize);
 
-    } else if(AttType > 1 && AttType <3){
-      let goose = new Goosling(AttName, AttEyes, AttHair, AttSize);
-      console.log(goose.AttName);
+
+    } else if(AttType === '2'){
+      goose = new Goosling(AttName, AttEyes, AttHair, AttSize);
+
       
 
-    }else if(AttType < 4 && AttType > 2){
-      let cat = new Puss(AttName, AttEyes, AttHair, AttSize);
-      console.log(cat.AttName);
+    }else if(AttType === '3'){
+      cat = new Puss(AttName, AttEyes, AttHair, AttSize);
+
       
     }
     else{
-      console.log('none');
+      console.log('Empty Pet');
     }
+
 
 });
 
+/// RETURN METHODS
+
 const RetB = document.querySelector("#modbehavior");
 const inpt = document.getElementById('family');
+const inpt1 = document.getElementById('movem');
+const inpt2 = document.getElementById('noise');
+
 RetB.addEventListener('click', function(RetBhv) {
   RetBhv.preventDefault();
 
-  const Owners = "JohnDoeTest"; //Method GetFamily();
-  inpt.innerHTML = Owners;
+let ptType = AttTypeE1.value;
+let owners="";
+let ready ="";
+let speak="";
+
+  if(ptType === '1'){
+  
+  owners = dog.AttName;
+  inpt.value = owners;
+
+  ready = `${AttNameE1.value} runs with a Bone`;//dog.GetTurnOn;
+  inpt1.value = ready;
+
+  speak = 'woof woof';
+  inpt2.value = speak;
+
+
+  }
+   else if(ptType === '2'){
+
+  owners = goose.AttName;//goose.Getfamily();
+  inpt.value = owners;
+
+  ready = `${AttNameE1.value} runs with a Carrot`;//goose.GetTurnOn();
+  inpt1.value = ready;
+
+  speak = 'Honk Honk'//goose.GetmakeNoise();
+  inpt2.value = speak;
+
+  }
+    else if(ptType === '3'){
+
+  owners = cat.AttName//cat.family();
+  inpt.value = owners;
+
+  ready = `${AttNameE1.value} runs with a Wool Hank`;//cat.GetTurnOn();
+  inpt1.value = ready;
+
+  speak = 'Miau Miau';
+  inpt2.value = speak;
+
+  }
+  else{
+    console.log("NOT TYPE IN BEHAVIOR");
+  }
 
   })
 
 
 
+  ///// OVERWRITE/OVERLOADING METHODS
 
+document.getElementById('Meth-Form').addEventListener('submit', e => {
+e.preventDefault();
+
+  let rltvs = familyTypeE2.value;
+  let lang = noiseE2.value;
+  let y = AttTypeE1.value;
+  
+     
+
+  if(y === '1'){
+
+     dog.setFamily(rltvs);
+     dog.setMakeNoise(lang);
+    
+
+    }
+  else if(y === '2'){
+ 
+    goose.setFamily(rltvs);
+    goose.setMakeNoise(lang);
+
+  }
+  else if(y === '3'){
+   cat.setFamily(rltvs);
+   cat.setMakeNoise(lang);
+
+  }   
+
+})
+
+/////// END
+endBtn.addEventListener('click', function(e) {
+e.preventDefault();
+document.getElementById("dogF").style.display = "block";
+paraphE1 = [dog.family,"This is ", dog.AttName, dog.noise];
+codedisplay2.innerHTML = paraphE1;     
+     
+          
+})
+
+
+////////////////////////  C L A S S E S /////////////////////////
 
 //The super class
 class PetShape {
@@ -57,108 +166,83 @@ class PetShape {
      this.AttEyes = AttEyes;
      this.AttHair = AttHair;
      this.AttSize = AttSize;
-     
 
    }
 
-/// stared being itself
+
    GetFamily(){
-     let family = this.AttName;
+     this.family = 'Itself';
      return family;
+
 
    }
    makeNoise() {
      this.noise = noise;
-     
-
-     /*if(petType === 'dog' noise ="Woof");
-     if(petType === 'cat' noise ="Meow");
-     if(petType === 'goose' noise ="Honk!");*/
-      return noise;
+     return noise;
          
   }
 
-   /*TurnOn(){
-     return `${this.name} in ON`;
-    if(petType === 'dog' run ="Scratching");
-     if(petType === 'cat' run ="Licking");
-     if(petType === 'goose' run ="Plucking");
-     
-    }*/
-
-  /* Fight(){
-    this.attack = attack;
-
-    if(petType === 'dog' attack ="Bite");
-    if(petType === 'cat' attack ="Scratch");
-    if(petType === 'goose' attack ="Peck");
-
-     return attack;
-     
-   }*/
    
  }
 
 
-
-//New instance of Dog based on requirements
+//Subclass Doggy 
  class Doggy extends PetShape{
    constructor(... args) {
      super(... args);
      
-   }
-   //adding LovedOnes  
-   GetFamily(){
-   let LovedOnes=[];
-   let family = `${this.AttName} ${LovedOnes}`;
-   return family;
-   }
-
-   makeNoise(){
-   let noise="woof, woof";
-   return noise;
-   
-   }
-
-   TurnOn(){
-   let holding = "Bone";
-   return `${super.AttName} with a ${holding}`;
-
-
-  }
-   Figth(){
-   
-   }
   
+   }
+   
+setFamily(family){
+this.family = family;
+
+}
+   GetFamily(){}
+
+setMakeNoise(noise){
+this.noise = noise;
+
+}
+
+GetMakeNoise(){}
+
+
+setTurnOn(move){
+  this.move = move||"Bone";
+  return `${this.AttName} runs with a ${super.move}`;   
+  }
+
+  GetTurnOn(){}
+ 
 
  }
  
- 
-  //New instance of Goose based on requirements
  class Goosling extends PetShape{
    constructor(... args) {
      super(... args);
      
    }
-   //adding LovedOnes  
+   setFamily(family){
+   this.family = family;
+
+   }
    GetFamily(){
    
    }
 
-   makeNoise(){
-   this.noise="Honk, Honk";
-   return noise;
+   GetMakeNoise(noise){
+   this.noise= noise;
+   
    
    }
    TurnOn(){
    let holding = "Carrot";
-   return `${super.TurnOn()} with a ${holding}`;
+   let move =  `${super.AttName} running with a ${holding}`;
+   return move;
 
 
  }
-   Figth(){
-   
-   }
 
  }
 
@@ -167,55 +251,123 @@ class PetShape {
      super(... args);
      
    }
-   //adding LovedOnes  
-   GetFamily(){
-   //this.loyal =  
-   }
+  setFamily(family){
+  this.family = family;
+
+  }
+   GetFamily(){}
 
    makeNoise(){
-   this.noise="Miau";
+   this.noise= noise;
+   return noise;
    
    }
 
    TurnOn(){
-   let holding = "Fishing Rod";
-   return `${super.name} with a ${holding}`;
+   let holding = "Wool Hank";
+   let move =  `${super.AttName} running with a ${holding}`;
+   return move;
 
 
   }
-   Figth(){
-   
-   }
   
 
  }
 
  /// Printing Classes and code
-
-const type = document.querySelector('#sltPet');
-const name = document.querySelector('#name');
-const eyeColor = document.querySelector('#eyes');
-const hairColor = document.querySelector('#hairColor');
-const size = document.querySelector('#sltsize');
-//const behavior1 = document.querySelector('#behavior1');
-const codedisplay = document.getElementById('footerOut');
-const flexCtr = document.querySelector('.flex-ctr');
-const createPetBtn = document.querySelector('#createPet');
+ 
   createPetBtn.addEventListener('click', function(e) {
        e.preventDefault();
-      const classTemplate = `class ${e.target.value} extends PetShape {
-        constructor(type,name,eyeColor,hairColor,size){
-        this.type = type;
-        this.name = name;
-        this.eyeColor = eyeColor;
-        this.hairColor = hairColor;
-        this.size = size;
+      document.getElementById("dogSk").style.display = "block";
+         
+      const classTemplate = `class PetShape {
+   constructor(AttName = 'no-name', AttEyes = 'grey', AttHair ='grey', AttSize = 'default') {
+     
+     this.AttName = AttName;
+     this.AttEyes = AttEyes;
+     this.AttHair = AttHair;
+     this.AttSize = AttSize;
 
-            } 
-        // ${type.value}() { }            
-         }`  
+   }
+
+
+   GetFamily(){
+     this.family = 'Itself';
+     return family;
+
+
+   }
+   makeNoise() {
+     this.noise = noise;
+     return noise;
+         
+  }
+
+   
+ }
+
+
+//Subclass Doggy 
+ class Doggy extends PetShape{
+   constructor(... args) {
+     super(... args);
+     
+  
+   }
+   
+setFamily(family){
+this.family = family;
+
+}
+   GetFamily(){}
+
+setMakeNoise(noise){
+this.noise = noise;
+
+}
+
+GetMakeNoise(){}
+
+
+setTurnOn(move){
+  this.move = move||"Bone";
+  return;   
+  }
+
+  GetTurnOn(){}
+ 
+
+ }
+ 
+ class Goosling extends PetShape{
+   constructor(... args) {
+     super(... args);
+     
+   }
+   setFamily(family){
+   this.family = family;
+
+   }
+   GetFamily(){
+   
+   }
+
+   GetMakeNoise(noise){
+   this.noise= noise;
+   
+   
+   }
+   TurnOn(){
+   let holding = "Carrot";
+   let move =  ;
+   return move;
+
+
+ }
+
+ }`  
             codedisplay.innerHTML = classTemplate;
-          //console.log(classTemplate);
+ 
             
         })
  
